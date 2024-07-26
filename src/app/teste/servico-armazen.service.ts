e82ca6994f8487244227a84cf61e87598008f96f
Samuel Lúcio <102866875+samucalucio1997@users.noreply.github.com>
2024-07-26 17:38:16 +0000

    mais um

--- src/app/teste/servico-armazen.service.ts
+++ src/app/teste/servico-armazen.service.ts
@@ -1,15 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicoArmazenService {
    
-  adicionarNome(lista:{nome:string , data:number}[],nome:string,data:number,id:number):void{
-    console.log(data);
-    lista.push({nome:nome, data:data});
-    id++;
+  
+  constructor( private listaService:HttpClient ) {}
+
+
+  adicionarNome(lista:{id:number ,name:string , email: string}[],nome:string,email:string,id:number):void{
+    lista.push({id:id, name:nome, email:email});
   }
+
+  preencherData():Observable<{id:number, name:string , email:string}[]>{
+    return this.listaService.get<{id:number, name:string , email:string}[]>("https://3.128.249.166:3000/clients"); 
+  } 
 
   
 }
--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServicoArmazenService } from './servico-armazen.service';
 
 @Component({
@@ -7,18 +7,28 @@ import { ServicoArmazenService } from './servico-armazen.service';
   templateUrl: './teste.component.html',
   styleUrl: './teste.component.css'
 })
-export class TesteComponent {
-   lista:{nome:string , data:number}[] = []
+export class TesteComponent implements OnInit{
+   lista:{id:number, name:string , email:string}[] = []
    nome='';
-   data:number = 0;
+   email:string = '';
    id:number = 0;
 
    constructor(private service: ServicoArmazenService) {} 
+   ngOnInit(){
+      this.preencherData();
+      console.log('Lista: ', this.lista);
+   }
 
    adicionarNome(){
       console.log('Adicionando nome: ', this.nome);
-      this.service.adicionarNome(this.lista,this.nome,this.data,this.id);
+      this.service.adicionarNome(this.lista,this.nome,this.email,this.id);
       this.nome = '';
-      this.data = 0;
+      this.email = '';
+   }
+
+   preencherData(){
+      this.service.preencherData().subscribe(data => {
+         this.lista = data;
+      });
    }
 }
